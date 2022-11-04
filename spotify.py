edfaa383910dea7ae3137f347958c3f8fc63cbb1
Sumeet Saini <sumeetsaini621@gmail.com>
2022-11-04 17:41:40 +0000

    changes secret format

--- spotify.py
+++ spotify.py
@@ -1,5 +1,20 @@
 import spotipy
+import json
 from spotipy.oauth2 import SpotifyClientCredentials
+
+global client_id
+global client_secret
+
+def get_secrets():
+    try:
+        s = json.load(open('secrets.json'))
+    except:
+        print('secrets.json not found')
+        exit()
+    client_id = s['client_id']
+    client_secret = s['client_secret']
+    print('Client Details Loaded!')
+
 
 path = 'spotify:track:'
 
@@ -14,3 +29,9 @@ result = spotify.track(path + data[0])
 album = result['album']
 artists = album['artists']
 print(result['name'], artists[0]['name'], album['release_date'].split('-')[0])
+
+def main():
+    get_secrets()
+
+if __name__ == "__main__":
+    main()
